63792489b3284642c0d62d04c5306070d9e69bb5
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-08-06 16:38:51 +0300

    Initial Commit

---
+++ SwiftUIPhotoPicker/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIPhotoPicker
+//
+//  Created by Atil Samancioglu on 6.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPhotoPicker/SwiftUIPhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhotoPickerApp.swift
+//  SwiftUIPhotoPicker
+//
+//  Created by Atil Samancioglu on 6.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
